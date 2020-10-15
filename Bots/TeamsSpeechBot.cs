ccebd06af3f48565d5a2f643d0eadc73cd9bb411
kimtth <iyai@naver.com>
2020-10-15 16:20:10 +0900

    first commit

---
+++ Bots/TeamsSpeechBot.cs
@@ -0,0 +1,311 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using SpeechAPI;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class TeamsSpeechBot : TeamsActivityHandler
+    {
+        private string _appId;
+        private string _appPassword;
+        private readonly IConfiguration _config;
+        private SpeechTextRecognizer speechRecognizer;
+
+        public TeamsSpeechBot(IConfiguration config)
+        {
+            _config = config;
+            _appId = config["MicrosoftAppId"];
+            _appPassword = config["MicrosoftAppPassword"];
+            speechRecognizer = new SpeechTextRecognizer(_config);
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            turnContext.Activity.RemoveRecipientMention();
+            var text = turnContext.Activity.Text.Trim().ToLower();
+
+            if (text.Contains("help") || text.Contains("menu"))
+            {
+                await MenuCardActivityAsync(turnContext, cancellationToken);
+            }
+            else if (text.Contains("start"))
+            {
+                await StartRecordActivityAsync(turnContext, cancellationToken);
+            }
+            else if (text.Contains("stop"))
+            {
+                await StopRecordActivityAsync(turnContext, cancellationToken);
+            }
+            else if (text.Contains("team"))
+            {
+                await TeamsFunctionActivityAsync(turnContext, cancellationToken);
+            }
+            else if (text.Contains("notice"))
+            {
+                await NotificationActivityAsync(turnContext, cancellationToken);
+            }
+            else
+            {
+                await MenuCardActivityAsync(turnContext, cancellationToken);
+            }
+        }
+
+        protected async Task NotificationActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var message = MessageFactory.Text("You got a notification.");
+            message.TeamsNotifyUser();
+
+            await turnContext.SendActivityAsync(message);
+        }
+
+        private async Task TeamsFunctionActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var member = new TeamsChannelAccount();
+
+            try
+            {
+                member = await TeamsInfo.GetMemberAsync(turnContext, turnContext.Activity.From.Id, cancellationToken);
+            }
+            catch (ErrorResponseException e)
+            {
+                if (e.Body.Error.Code.Equals("MemberNotFoundInConversation"))
+                {
+                    await turnContext.SendActivityAsync("Member not found.");
+                    return;
+                }
+                else
+                {
+                    throw e;
+                }
+            }
+
+            var message = MessageFactory.Text($"You are: {member.Name}.");
+            await turnContext.SendActivityAsync(message);
+        }
+
+        private async Task StartRecordActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await speechRecognizer.RecognizeSpeechContinualAsyncStart(turnContext);
+
+            var message = MessageFactory.Text($"Start Recording -----");
+            await turnContext.SendActivityAsync(message);
+        }
+
+        private async Task StopRecordActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if(speechRecognizer != null)
+                await speechRecognizer.RecognizeSpeechContinualAsyncStop();
+
+            var message = MessageFactory.Text($"Stop Recording -----");
+            await turnContext.SendActivityAsync(message);
+        }
+
+        private async Task MenuCardActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var card = new HeroCard
+            {
+                Buttons = new List<CardAction>
+                        {
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Start Recording",
+                                Text = "Start Recording"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Stop Recording",
+                                Text = "Stop Recording"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Help",
+                                Text = "Help"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Teams Me",
+                                Text = "Team"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Teams Notification",
+                                Text = "Notice"
+                            }
+                        }
+            };
+
+            await SendMenuCard(turnContext, card, cancellationToken);
+        }
+
+        private async Task SendMenuCard(ITurnContext<IMessageActivity> turnContext, HeroCard card, CancellationToken cancellationToken)
+        {
+            card.Title = "Welcome!";
+
+            var activity = MessageFactory.Attachment(card.ToAttachment());
+
+            await turnContext.SendActivityAsync(activity, cancellationToken);
+        }
+
+        protected override async Task OnTeamsMembersAddedAsync(IList<TeamsChannelAccount> membersAdded, TeamInfo teamInfo, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var teamMember in membersAdded)
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome to access {teamMember.GivenName} {teamMember.Surname}."), cancellationToken);
+            }
+        }
+
+        //private async Task GetSingleMemberAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        //{
+        //    var member = new TeamsChannelAccount();
+
+        //    try
+        //    {
+        //        member = await TeamsInfo.GetMemberAsync(turnContext, turnContext.Activity.From.Id, cancellationToken);
+        //    }
+        //    catch (ErrorResponseException e)
+        //    {
+        //        if (e.Body.Error.Code.Equals("MemberNotFoundInConversation"))
+        //        {
+        //            await turnContext.SendActivityAsync("Member not found.");
+        //            return;
+        //        }
+        //        else
+        //        {
+        //            throw e;
+        //        }
+        //    }
+
+        //    var message = MessageFactory.Text($"You are: {member.Name}.");
+        //    var res = await turnContext.SendActivityAsync(message);
+
+        //}
+
+        //private async Task DeleteCardActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        //{
+        //    await turnContext.DeleteActivityAsync(turnContext.Activity.ReplyToId, cancellationToken);
+        //}
+
+        //// If you encounter permission-related errors when sending this message, see
+        //// https://aka.ms/BotTrustServiceUrl
+        //private async Task MessageAllMembersAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        //{
+        //    var teamsChannelId = turnContext.Activity.TeamsGetChannelId();
+        //    var serviceUrl = turnContext.Activity.ServiceUrl;
+        //    var credentials = new MicrosoftAppCredentials(_appId, _appPassword);
+        //    ConversationReference conversationReference = null;
+
+        //    var members = await GetPagedMembers(turnContext, cancellationToken);
+
+        //    foreach (var teamMember in members)
+        //    {
+        //        var proactiveMessage = MessageFactory.Text($"Hello {teamMember.GivenName} {teamMember.Surname}. I'm a Teams conversation bot.");
+
+        //        var conversationParameters = new ConversationParameters
+        //        {
+        //            IsGroup = false,
+        //            Bot = turnContext.Activity.Recipient,
+        //            Members = new ChannelAccount[] { teamMember },
+        //            TenantId = turnContext.Activity.Conversation.TenantId,
+        //        };
+
+        //        await ((BotFrameworkAdapter)turnContext.Adapter).CreateConversationAsync(
+        //            teamsChannelId,
+        //            serviceUrl,
+        //            credentials,
+        //            conversationParameters,
+        //            async (t1, c1) =>
+        //            {
+        //                conversationReference = t1.Activity.GetConversationReference();
+        //                await ((BotFrameworkAdapter)turnContext.Adapter).ContinueConversationAsync(
+        //                    _appId,
+        //                    conversationReference,
+        //                    async (t2, c2) =>
+        //                    {
+        //                        await t2.SendActivityAsync(proactiveMessage, c2);
+        //                    },
+        //                    cancellationToken);
+        //            },
+        //            cancellationToken);
+        //    }
+
+        //    await turnContext.SendActivityAsync(MessageFactory.Text("All messages have been sent."), cancellationToken);
+        //}
+
+        //private static async Task<List<TeamsChannelAccount>> GetPagedMembers(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        //{
+        //    var members = new List<TeamsChannelAccount>();
+        //    string continuationToken = null;
+
+        //    do
+        //    {
+        //        var currentPage = await TeamsInfo.GetPagedMembersAsync(turnContext, 100, continuationToken, cancellationToken);
+        //        continuationToken = currentPage.ContinuationToken;
+        //        members = members.Concat(currentPage.Members).ToList();
+        //    }
+        //    while (continuationToken != null);
+
+        //    return members;
+        //}
+
+        //private static async Task SendWelcomeCard(ITurnContext<IMessageActivity> turnContext, HeroCard card, CancellationToken cancellationToken)
+        //{
+        //    var initialValue = new JObject { { "count", 0 } };
+        //    card.Title = "Welcome!";
+        //    card.Buttons.Add(new CardAction
+        //    {
+        //        Type = ActionTypes.MessageBack,
+        //        Title = "Update Card",
+        //        Text = "UpdateCardAction",
+        //        Value = initialValue
+        //    });
+
+        //    var activity = MessageFactory.Attachment(card.ToAttachment());
+
+        //    await turnContext.SendActivityAsync(activity, cancellationToken);
+        //}
+
+        private static async Task SendUpdatedCard(ITurnContext<IMessageActivity> turnContext, HeroCard card, CancellationToken cancellationToken)
+        {
+            card.Title = "I've been updated";
+
+            var data = turnContext.Activity.Value as JObject;
+            data = JObject.FromObject(data);
+            data["count"] = data["count"].Value<int>() + 1;
+            card.Text = $"Update count - {data["count"].Value<int>()}";
+
+            card.Buttons.Add(new CardAction
+            {
+                Type = ActionTypes.MessageBack,
+                Title = "Update Card",
+                Text = "UpdateCardAction",
+                Value = data
+            });
+
+            var activity = MessageFactory.Attachment(card.ToAttachment());
+            activity.Id = turnContext.Activity.ReplyToId;
+
+            await turnContext.UpdateActivityAsync(activity, cancellationToken);
+        }
+
+    }
+}
---
+++ Services/SpeechToText.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.Extensions.Configuration;
+
+namespace SpeechAPI
+{
+    class SpeechTextRecognizer
+    {
+        private static SpeechRecognizer speechRecognizer;
+        private string _speechSubscriptionKey;
+        private string _speechServiceRegion;
+        public delegate void SendMessageDelegate(string msg);
+
+        public SpeechTextRecognizer(IConfiguration config)
+        {
+            _speechSubscriptionKey = config["SpeechSubscriptionKey"];
+            _speechServiceRegion = config["SpeechServiceRegion"];
+        }
+
+        public async Task RecognizeSpeechOnceAsync()
+        {
+            var config = SpeechConfig.FromSubscription(_speechSubscriptionKey, _speechServiceRegion);
+
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                var result = await recognizer.RecognizeOnceAsync();
+
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"We recognized: {result.Text}");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+            }
+        }
+        
+        public async Task RecognizeSpeechContinualAsyncStart(ITurnContext<IMessageActivity> turnContext)
+        {
+            var config = SpeechConfig.FromSubscription(_speechSubscriptionKey, _speechServiceRegion);
+            string[] languages = { "ja-JP", "en-US", "en-IN", "en-GB" };
+            var language = AutoDetectSourceLanguageConfig.FromLanguages(languages);
+            var stopRecognition = new TaskCompletionSource<int>();
+
+            SendMessageDelegate msgDelegate = (string message) =>
+            {
+                turnContext.SendActivityAsync(message);
+            };
+
+            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            {
+
+                using (var recognizer = new SpeechRecognizer(config, language, audioConfig))
+                {
+                    // Subscribes to events.
+                    recognizer.Recognizing += (s, e) =>
+                    {
+                        Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+                    };
+
+                    recognizer.Recognized += (s, e) =>
+                    {
+                        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                        {
+                            string message = e.Result.Text;
+                            msgDelegate(message);
+                            Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                        }
+                        else if (e.Result.Reason == ResultReason.NoMatch)
+                        {
+                            Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        }
+                    };
+
+                    recognizer.Canceled += (s, e) =>
+                    {
+                        Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                        if (e.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    recognizer.SessionStarted += (s, e) =>
+                    {
+                        Console.WriteLine("\n    Session started event.");
+                    };
+
+                    recognizer.SessionStopped += (s, e) =>
+                    {
+                        Console.WriteLine("\n    Session stopped event.");
+                        Console.WriteLine("\nStop recognition.");
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+                    await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                    // Waits for completion.
+                    // Use Task.WaitAny to keep the task rooted.
+                    Task.WaitAny(new[] { stopRecognition.Task });
+
+                    speechRecognizer = recognizer;
+                }
+            }
+        }
+
+        public async Task RecognizeSpeechContinualAsyncStop()
+        {
+            if(speechRecognizer != null)
+            {
+                // Stops recognition.
+                await speechRecognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+            }
+
+        }
+
+    }
+}
